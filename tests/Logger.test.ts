3b52926f2e3f35b2d0080af7b9915f916a5e6629
Bhushan <bhushangaykawad@gmail.com>
2022-07-17 19:44:27 +0530

    formatting stuff and ts-jest configs added

--- tests/Logger.test.ts
+++ tests/Logger.test.ts
@@ -1,15 +1,14 @@
-import {getLogger} from '../src';
-import {DRIVERS} from '../src/types';
-import FileLogger from "../src/FileLogger";
-import ConsoleLogger from "../src/ConsoleLogger";
-
+import { getLogger } from '../src';
+import { DRIVERS } from '../src/types';
+import FileLogger from '../src/FileLogger';
+import ConsoleLogger from '../src/ConsoleLogger';
 
 test('default logger is file logger', () => {
-    const log = getLogger();
-    expect(log).toBeInstanceOf(ConsoleLogger)
+  const log = getLogger();
+  expect(log).toBeInstanceOf(ConsoleLogger);
 });
 
 test('when needed file logger can be used', () => {
-    const log = getLogger(DRIVERS.FILE)
-    expect(log).toBeInstanceOf(FileLogger)
+  const log = getLogger(DRIVERS.FILE);
+  expect(log).toBeInstanceOf(FileLogger);
 });
